4ac69d8fe0c308f006be8370f4c93ff6b44996ca
Zestlifia <zestlifia123@gmail.com>
2020-05-13 23:48:56 +0800

    remove reduntant

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
-// import AppHeader from './apps/AppHeader'
-import Counter from './apps/Counter';
-import ProfileContainer from './counterComponent/ProfileContainer';
 import DailyDrinks from './apps/DailyDrinks';
 
 function App() {
   return (
     <div className="App">
-      {/* <AppHeader /> */}
-      <Counter />
-      <ProfileContainer />
       <DailyDrinks />
     </div>
   );
